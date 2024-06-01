7e73167a2e43c318bafd20d18ece2bb59b709130
FIDELIS-TUWEi <fideliofidel9@gmail.com>
2024-06-01 20:03:46 +0300

    added put, post, get, route logic

--- routes/participant.routes.js
+++ routes/participant.routes.js
@@ -1,10 +1,12 @@
 const express = require("express");
-const { createParticipant, getParticipants, getParticipant, editParticipant } = require("../controller/participant.controller");
+const { createParticipant, getParticipants, getParticipant, editParticipant, deleteParticipant } = require("../controller/participant.controller");
 const router = express.Router();
 
 router.post("/", createParticipant);
 router.get("/", getParticipants);
 router.get("/:id", getParticipant);
 router.put("/:id", editParticipant);
+router.delete("/:id", deleteParticipant);
+
 
 module.exports = router;
