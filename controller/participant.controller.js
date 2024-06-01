6b1d568df5c3284b72e8bd01f99f4a69954ba566
FIDELIS-TUWEi <fideliofidel9@gmail.com>
2024-06-01 20:00:12 +0300

    added put, post, get, logic

--- controller/participant.controller.js
+++ controller/participant.controller.js
@@ -1,8 +1,9 @@
 const Participant = require("../model/participant.model");
 
+// @desc Create new Participant
 const createParticipant = async (req, res) => {
     try {
-        const { firstName, lastName, username } = req.body;
+        const { name } = req.body;
 
         // 1. check if participant exists
         const duplicate = await Participant.findOne({ username });
@@ -13,9 +14,7 @@ const createParticipant = async (req, res) => {
 
         // 2. create new participant
         const newParticipant = new Participant({
-            firstName,
-            lastName,
-            username
+            name
         });
 
         if (newParticipant) {
@@ -28,11 +27,73 @@ const createParticipant = async (req, res) => {
         res.status(201).json(newParticipant);
 
     } catch (error) {
-        logger.error("Error occured in newParticipant controller", error);
+        logger.error("Error occured on newParticipant controller", error);
         res.status(500).json({ error: "Internal Server Error" });
     }
 };
 
+// @desc All Participants
+const getParticipants = async (req, res) => {
+    try {
+        const participants = await Participant.find({}).lean();
+
+        if (!participants) {
+            return res.status(404).json({ error: "No Participants found!" });
+        };
+
+        res.status(200).json(participants);
+
+    } catch (error) {
+        logger.error("Error occured on getParticipants controller", error);
+        res.status(500).json({ error: "Internal Server Error" });
+    }
+};
+
+// @desc Single Participant
+const getParticipant = async (req, res) => {
+    try {
+        const participantId = req.params.id;
+        const participant = await Participant.findById(participantId);
+
+        if (!participant) {
+            return res.status(404).json({ error: `Participant with ID: ${participantId} not found!` });
+        };
+
+        res.status(200).json(participant);
+        
+    } catch (error) {
+        logger.error("Error occured on getParticipant controller", error);
+        res.status(500).json({ error: "Internal Server Error" });
+    }
+};
+
+// @desc Update participant
+const editParticipant = async (req, res) => {
+    try {
+        const participantId = req.params.id;
+        // check if participant exists
+        const participant = await Participant.findById(participantId);
+
+        if (participant) {
+            await Participant.findByIdAndUpdate(participantId, req.body, { new: true, runValidators: true });
+        } else {
+            return res.status(404).json({ error: "Participant not found!" });
+        }
+
+        res.status(200).json(participant);
+
+    } catch (error) {
+        logger.error("Error occured on editParticipant controller", error);
+        res.status(500).json({ error: "Internal Server Error" });
+    }
+};
+
+// @desc Delete Participant
+
+
 module.exports = {
-    createParticipant
+    createParticipant,
+    getParticipants,
+    getParticipant,
+    editParticipant
 };
